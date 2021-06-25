a182b34577c305499af80239eeab5f706e7cabbc
Solai Raj <msraj085@gmail.com>
2021-06-25 20:03:50 +0530

    added star and implemented random maps

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@
 
 import dp from "./sprites/sola.png";
 import platform from "./sprites/platform.png";
+import star from "./sprites/star.png";
 import ladder from "./sprites/ladder.png";
 import { players } from "./multiplayer";
 import { config } from "./config";
-import { k1 } from "./levels";
+import { maps } from "./levels";
 
 const k = kaboom(config);
 k.loadSprite("dp", dp);
-k.loadSprite("platform",platform);
+k.loadSprite("star", star);
+k.loadSprite("platform", platform);
 k.loadSprite("ladder", ladder);
 
 // data structures
@@ -18,46 +20,41 @@ let player_avatars = [];
 
 // main scene
 k.scene("main", () => {
+  // main level
+  const randomMap = maps[Math.floor(Math.random() * maps.length)]; //The Random map 
+  k.addLevel(randomMap, {
+    height: 150,
+    width: 50,
+    "-": [k.sprite("platform"), k.scale(0.1), k.solid(), "platform"],
+    "|": [k.sprite("platform"), k.scale(0.1), k.solid(), "platform"],
+    //TODO: add declaration for '?' blocks
+  });
 
+  // adding all players
+  let pointer = 0;
+  players.forEach((player) => {
+    const temp = k.add([
+      k.sprite("dp"),
+      k.scale(0.25),
+      k.pos(pointer, 0),
+      k.body({ jumpForce: 640 }),
+      player,
+    ]);
+    player_avatars.push(temp);
+    pointer += 300;
+  });
 
-    // main level 
-    k.addLevel([
-        "                    ",
-        "   -----        --- ", 
-        "          --        ",
-        "--------------------"
-    ],{
-        height:150,
-        width:50,
-        '-': [k.sprite('platform'),k.scale(0.10),k.solid(),'platform'],
-    });
-
-    // adding all players
-    let pointer = 0;
-    players.forEach(player=>{
-        const temp = k.add([
-            k.sprite('dp'),
-            k.scale(0.25),
-            k.pos(pointer,0),
-            k.body({jumpForce: 640,}),
-            player,
-        ]);
-        player_avatars.push(temp);
-        pointer+=300;
-    });
-
-    // key controls
-    let myplayer = 0; // testing 
-    k.keyPress('space',()=>{
-        player_avatars[myplayer].jump(300);
-    });
-    k.keyDown('left',()=>{
-        player_avatars[myplayer].move(-300,0);
-    });
-    k.keyDown('right',()=>{
-        player_avatars[myplayer].move(300,0);
-    });
-
+  // key controls
+  let myplayer = 0; // testing
+  k.keyPress("space", () => {
+    player_avatars[myplayer].jump(300);
+  });
+  k.keyDown("left", () => {
+    player_avatars[myplayer].move(-300, 0);
+  });
+  k.keyDown("right", () => {
+    player_avatars[myplayer].move(300, 0);
+  });
 });
 
 // start the game
